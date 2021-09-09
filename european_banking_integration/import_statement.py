ddca127a95eb0776ac7721db4189b5e14dcdd06c
al-chaudhari <chaudharia041@gmail.com>
2021-09-09 19:51:59 +0530

    feat: added regular imports

---
+++ european_banking_integration/import_statement.py
@@ -0,0 +1,7 @@
+import frappe
+import csv
+from frappe.utils import get_site_name
+from european_banking_integration.utils import get_file_path, get_party, get_paid_from, account_paid_to
+from os import path
+from european_banking_integration.reader import Reader
+from european_banking_integration.transformer import Kreissparkasse_transformer
